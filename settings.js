4d415cade029f40203cc70132356eb5e8f4c2328
Deutscherschuh <paper.io322@gmail.com>
2024-11-08 02:27:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'mert ',
   botName: "Ayush Bot",
   botOwner: 'vk', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '67581398175', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
